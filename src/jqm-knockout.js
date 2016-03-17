f804f146e3427db8675576ea3e402709f3796916
chunchill <chunchill@163.com>
2016-03-17 22:37:47 +0800

    listview html files.

--- src/jqm-knockout.js
+++ src/jqm-knockout.js
@@ -291,6 +291,38 @@
 
     };
 
+    ko.virtualElements.allowedBindings.jqmTemplate = true;
+    ko.virtualElements.allowedBindings.jqmRefreshList = true;
+    ko.bindingHandlers.jqmTemplate = {
+        init: ko.bindingHandlers.template.init,
+        update: function (element, valueAccessor, allBindingsAccessor, viewModel, context) {
+            ko.bindingHandlers.template.update(element, valueAccessor,
+                allBindingsAccessor, viewModel, context);
+            try {
+                $(element).listview('refresh');
+            } catch (e) {
+                //$(element).listview();
+            }
+        }
+    };
+
+    ko.bindingHandlers.jqmRefreshList = {
+        update: function (element, valueAccessor) {
+
+            ko.utils.unwrapObservable(valueAccessor()); //just to create a dependency
+            setTimeout(function () { //To make sure the refresh fires after the DOM is updated
+
+                try {
+                    $(element).listview();
+                    $(element).listview('refresh');
+                } catch (e) {
+                    //$(element).listview();
+                }
+
+            }, 0);
+        }
+    };
+
 
     function createAttributeBindings(ko,attr){
         var dataAttr = 'data-'+ attr;
